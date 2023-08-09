4af3aa3f9964abdfac932604a205e77b5d002f11
DMITRYSHVED <dima.shved.0202@mail.ru>
2023-08-09 09:12:20 +0300

    COMMIT

--- src/main/java/com/warehouse/validator/DiscardValidator.java
+++ src/main/java/com/warehouse/validator/DiscardValidator.java
@@ -2,6 +2,7 @@ package com.warehouse.validator;
 
 import com.warehouse.dto.StorageDTO;
 import com.warehouse.entity.Storage;
+import com.warehouse.manager.ProductOrderManager;
 import com.warehouse.manager.StorageManager;
 import com.warehouse.util.ValidationConstants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ public class DiscardValidator implements Validator {
     @Autowired
     private StorageManager storageManager;
 
+    @Autowired
+    private ProductOrderManager productOrderManager;
+
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -25,12 +29,24 @@ public class DiscardValidator implements Validator {
     public void validate(Object target, Errors errors) {
         StorageDTO storageDTO = (StorageDTO) target;
         Storage storage = storageManager.getById(storageDTO.getStorageId());
+        int orderedQuantity;
 
         if (storageDTO.getQuantity() < 0) {
             errors.rejectValue("quantity", "NEGATIVELY", ValidationConstants.POSITIVE_QUANTITY);
         }
         if (storage.getQuantity() < storageDTO.getQuantity()) {
             errors.rejectValue("quantity", "DEFICIENCY", ValidationConstants.OUT_OF_STOCK);
+        }
+
+        orderedQuantity = 0;
+        for (int i = 0; i < productOrderManager.getOrderedProducts().size(); i++) {
+            if (storage.getProduct().getId() == productOrderManager.getOrderedProducts().get(i).getProduct().getId()) {
+                orderedQuantity += productOrderManager.getOrderedProducts().get(i).getQuantity();
+            }
+        }
+        if (storageDTO.getQuantity() > (storage.getQuantity() - orderedQuantity)) {
+            errors.rejectValue("quantity", "DEFICIENCY", "нельзя списать заказанный товар," +
+                    "отмените заказ, и попробуйте еще раз!");
         }
     }
 }
